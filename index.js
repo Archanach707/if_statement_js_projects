44dea3b854801bcb9ce63daedfdec1fa13bd560b
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2024-10-02 18:20:40 +0530

    used logical  && operator

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+// without prompt user name and age
+const userName = "Archana";
+const userAge = 30;
+
+console.log(`Name: ${userName}`);
+console.log(`Age: ${userAge}`);
+
+if (userAge >= 18 && userAge <= 30) {
+  console.log("User is a college student.");
+  console.log('Ans he/she learning computer science');
+}
+
+
+console.log("Program Ended!!!");
