716a44b06786436515316faea22b2c495139f7eb
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2024-10-02 19:17:41 +0530

    if statement exapmles

--- index.js
+++ index.js
@@ -6,8 +6,29 @@ console.log(`Name: ${userName}`);
 console.log(`Age: ${userAge}`);
 
 if (userAge >= 18 && userAge <= 30) {
-  console.log("User is a college student.");
-  console.log('Ans he/she learning computer science');
+  console.log(`${userName} is a college student.`);
+  console.log('And he/she learning computer science');
+}
+
+const userName1 = prompt("Please enter a name");
+const userAge1 = prompt("Please enter a age");
+
+console.log(`Name: ${userName1}`);
+console.log(`Age: ${userAge1}`);
+
+if (userAge1 >= 18 && userAge1 <= 24) {
+  console.log(`${userName1} is a college student.`);
+  console.log('And he/she learning computer science');
+}
+
+if (userAge1 >= 25 && userAge1 <= 45) {
+  console.log(`${userName1} is a working professional.`);
+  console.log('And he/she a web developer');
+}
+
+if (userAge1 > 45) {
+  console.log(`${userName1} is retired.`);
+  console.log('And he/she read newspaper');
 }
 
 
