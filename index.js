28efaf17eeb66b1fb7c1aac6caa007b2cee676a2
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2024-10-04 18:28:42 +0530

    also make else if condition statment project

--- index.js
+++ index.js
@@ -10,8 +10,8 @@
 //   console.log('And he/she learning computer science');
 // }
 
-const userName1 = prompt("Please enter a name");
-const userAge1 = prompt("Please enter a age");
+const userName1 = prompt("Please enter a name") || "Archana Chauhan";
+const userAge1 = prompt("Please enter a age") || 22;
 
 console.log(`Name: ${userName1}`);
 console.log(`Age: ${userAge1}`);
@@ -21,25 +21,33 @@ if (userAge1 >= 0 && userAge1 <= 4) {
   console.log('And he/she playing.');
 }
 
-if (userAge1 >= 5 && userAge1 <= 17) {
+else if (userAge1 >= 5 && userAge1 <= 17) {
   console.log(`${userName1} is a school student.`);
   console.log('And he/she learning science and maths');
 }
 
-if (userAge1 >= 18 && userAge1 <= 24) {
+else if (userAge1 >= 18 && userAge1 <= 24) {
   console.log(`${userName1} is a college student.`);
   console.log('And he/she learning computer science');
 }
 
-if (userAge1 >= 25 && userAge1 <= 45) {
+else if (userAge1 >= 25 && userAge1 <= 45) {
   console.log(`${userName1} is a working professional.`);
   console.log('And he/she a web developer');
 }
 
-if (userAge1 > 45) {
+else if (userAge1 > 45 && userAge1 < 121) {
   console.log(`${userName1} is retired.`);
   console.log('And he/she read newspaper');
 }
 
+else if (userAge1 > 121) {
+  console.log(`${userName1} is immortal.`);
+  console.log('And he/she read novels');
+}
+
+else if (userAge1 < 0) {
+  console.log('Please enter a valid age');
+}
 
 console.log("Program Ended!!!");
